4409c62f3470dba43945bd32224c4972c18cb329
PARPSY1122 <119403673+PARPSY1122@users.noreply.github.com>
2023-10-06 11:11:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "jenkins-cicd1"
     key            = "my-terraform-environment/main"
-    region         = "US East (Ohio) us-east-2"
+    region         = "us-east-2"
     dynamodb_table = "ajay-dynamo-db-table"
   }
 }
