5019855fc8c22be1a98a8eeec06401eebee89fd1
ravivarma12345 <113004696+ravivarma12345@users.noreply.github.com>
2023-10-16 11:40:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "ap-south-1a"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "ansadmin"
+  default     = "rravi"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,10 +12,10 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-024e6efaf93d85776"
+  default     = "ami-0f5ee92e2d63afc18"
 }
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "ajayreddy-bucket-peddireddy"
+  default     = "leo-bucket-peddireddy"
 }
